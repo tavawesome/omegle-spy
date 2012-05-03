9d74dc1a04f2cd95b1e139f4fa1e08d4277ed56f
sfoley1988 <sfoley1988@34cf4260-b807-1d74-2da7-48fc740f640c>
2012-05-03 20:21:02 +0000

    Retabbed source files from tabs to 4 spaces. Now I can avoid Google Code's stupidity

--- old/filter.pl
+++ old/filter.pl
@@ -7,41 +7,41 @@ $b = 'Beta';
 $opt_regex = qr/^-(.+)/;
 if (exists($ARGV[0]))
 {
-	$c = $ARGV[0];
-	if ($c eq '-')
-	{
-		shift @ARGV;
-		($a,$b) = ($b,$a);
-	}
-	elsif ($c =~ /$opt_regex/)
-	{
-		shift @ARGV;
-		$a = $1;
-		
-		if (exists($ARGV[0]))
-		{
-			$c = $ARGV[0];
-			if ($c =~ /$opt_regex/)
-			{
-				shift @ARGV;
-				$b = $1;
-			}
-		}
-	}
+    $c = $ARGV[0];
+    if ($c eq '-')
+    {
+        shift @ARGV;
+        ($a,$b) = ($b,$a);
+    }
+    elsif ($c =~ /$opt_regex/)
+    {
+        shift @ARGV;
+        $a = $1;
+        
+        if (exists($ARGV[0]))
+        {
+            $c = $ARGV[0];
+            if ($c =~ /$opt_regex/)
+            {
+                shift @ARGV;
+                $b = $1;
+            }
+        }
+    }
 }
 
 while (<>)
 {
-	if ($_ =~ /^$ts /)
-	{
-		if (($_ =~ s/^$ts (?:$b|\{\{from $b\}\})/Stranger/) ||
-			($_ =~ s/^$ts (?:$a|<<$a>>)/You/))
-		{
-			print;
-		}
-	}
-	else
-	{
-		print;
-	}
+    if ($_ =~ /^$ts /)
+    {
+        if (($_ =~ s/^$ts (?:$b|\{\{from $b\}\})/Stranger/) ||
+            ($_ =~ s/^$ts (?:$a|<<$a>>)/You/))
+        {
+            print;
+        }
+    }
+    else
+    {
+        print;
+    }
 }
